85f5e0dce8c761a443d350ac6e3b7fdd8bd28e16
Pro Amedeo <proamedeo@MacBook-Pro-di-Pro.local>
2018-11-21 17:40:23 +0100

    ok

--- script.js
+++ script.js
@@ -3,3 +3,20 @@
 // - Restituisce il testo con xxx al posto delle parole censurate.
 // - Stampa un "badword index" calcolato come il numero di
 // parole censurate su il numero di parole totali
+
+// mi creo un array con le parole da censurare
+var array = ['non','ciao','chiamo']
+// faccio inserire il testo all'utente
+var fraseUtente = prompt('inserisci una frase');
+// cerco nel testo se ci sono parole da censurare
+for (var i = 0; i < array.length; i++) {
+
+  if (fraseUtente.includes(array[i])) {
+
+    fraseUtente = fraseUtente.replace(array[i], 'xxx');
+
+  }
+
+}
+
+console.log(fraseUtente);
